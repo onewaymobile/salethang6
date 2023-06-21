adf56656a60ae2fe689f3c0dc46f1110c85711af
MyVanThan <minhculong2509@gmail.com>
2023-06-21 20:47:30 +0700

    test footer

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 var hideBtns = document.querySelectorAll(".open__hide");
+var footerShowBtns = document.querySelectorAll(".footer__show--btn");
 
 hideBtns.forEach(function(hideBtn){
   hideBtn.onclick = function(){
     var parentElement = hideBtn.parentNode;
     var hideEle = parentElement.querySelector(".row__hide");
+    hideEle.classList.add('show-on-pc')
+    hideBtn.classList.add('hide_on_pc')
+  }
+})
+
+footerShowBtns.forEach(function(footerShowBtn){
+  footerShowBtn.onclick = function(){
+    var footerShowItem = footerShowBtn.querySelector(".footer__show--item");
     hideEle.classList.add('show-on-pc')
     hideBtn.classList.add('hide_on_pc')
   }
