11c64175cf14d26e87e9b679ad8bbe98660323ae
MyVanThan <minhculong2509@gmail.com>
2023-08-06 18:53:44 +0700

    fix nhieu

--- main.js
+++ main.js
@@ -24,15 +24,33 @@ hideBtns.forEach(function(hideBtn){
   hideBtn.onclick = function(){
     var parentElement = hideBtn.parentNode;
     var hideEle = parentElement.querySelector(".row__hide");
-    if(hideEle.classList.contains('show-on-pc')){
-      console.log("close")
-      hideEle.classList.remove('show-on-pc');
-      hideBtn.innerHTML = "Xem Thêm"
-    } else {
-      console.log("open")
-      hideEle.classList.add('show-on-pc');
-      hideBtn.innerHTML = "Đóng"
+    var hideEleMbs = parentElement.querySelectorAll(".row__hide-mb");
+    if(hideEle){
+      if(hideEle.classList.contains('show-on-pc')){
+        console.log("close")
+        hideEle.classList.remove('show-on-pc');
+        hideBtn.innerHTML = "Xem Thêm"
+      } else {
+        console.log("open")
+        hideEle.classList.add('show-on-pc');
+        hideBtn.innerHTML = "Đóng"
+      }
     }
+
+    hideEleMbs.forEach(function(hideEleMb){
+      if(hideEleMb.classList.contains('show')){
+        hideEleMb.classList.remove('show');
+        if(!hideEle){
+          hideBtn.innerHTML = "Xem Thêm"
+        }
+      } else {
+        hideEleMb.classList.add('show');
+        if(!hideEle){
+          hideBtn.innerHTML = "Đóng"
+        }
+      }
+    })
+
   }
 })
 footerShowParents.forEach(function(footerShowParent){
